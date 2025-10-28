a63df86096d243f022a58dc9fb81a7696a8774aa
xinbinhan <50063263+xinbinhan@users.noreply.github.com>
2025-10-28 18:01:39 +0800

    Update print statement from 'Hello' to 'Goodbye'

--- tool.py
+++ tool.py
@@ -293,7 +293,7 @@ def prefixStr(nodelist,prestr):
 def getResponse(url, custom_user_agent=None):
     response = None
     headers = {
-        'User-Agent': custom_user_agent if custom_user_agent else 'v2rayng'
+        'User-Agent': 'v2rayng'
         #'User-Agent': custom_user_agent if custom_user_agent else 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.5 Safari/605.1.15'
         #'User-Agent': 'clash.meta'
     }
@@ -335,5 +335,6 @@ class ConfigSSH:
 
     def close(self):
         self.ssh.close()
+
 
 
